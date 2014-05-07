b6eb44ff9183ff0357af61469080c691514d015d
Wes Mangum <wesmangum24@gmail.com>
2014-05-07 09:32:58 -0500

    initial commit

---
+++ app/lib/init-routes.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var traceur = require('traceur');
+var initialized = false;
+
+module.exports = (req, res, next)=>{
+  if(!initialized){
+    initialized = true;
+    load(req.app, next);
+  }else{
+    next();
+  }
+};
+
+function load(app, fn){
+  var home = traceur.require(__dirname + '/../routes/home.js');
+
+  app.get('/', home.index);
+  app.get('/help', home.help);
+  console.log('Routes Loaded');
+  fn();
+}
